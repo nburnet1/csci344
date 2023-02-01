803f93de3b5ad2c7f52ec2029e39641826a32cc4
nburnet1 <nburnet1@unca.edu>
2023-02-01 14:50:30 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   lectures/lecture07/01-style-property-demo/index.html
    	modified:   lectures/lecture07/01-style-property-demo/main.js

--- lectures/lecture07/01-style-property-demo/main.js
+++ lectures/lecture07/01-style-property-demo/main.js
@@ -2,21 +2,34 @@
 const makeRed = () => {
     // your code here...
     console.log('Change background to red');
-    document.querySelector('body').style.backgroundColor = 'red';
+    document.querySelector('#section1').style.backgroundColor = 'red';
 };
 
 const makeBlue = () => {
     // your code here...
     console.log('Change background to blue');
+    document.querySelector('#section2').style.backgroundColor = 'blue';
+
+
 };
 
 const makePink = () => {
     // your code here...
     console.log('Change background to pink');
+    document.querySelector('#section3').style.backgroundColor = 'pink';
 };
 
 const makeOrange = () => {
     // your code here...
     console.log('Change background to orange');
+    document.querySelector('#section4').style.backgroundColor = 'orange';
 };
+
+const makeRainbow = () => {
+    const rain = document.querySelectorAll('.my-section');
+
+    for(var i = 0; i < rain.length; i ++){
+        rain[i].style.backgroundColor = 'grey';
+    }
+}
 
