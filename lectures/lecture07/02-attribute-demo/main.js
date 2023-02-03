a2deab09601263668dcd66765b3f6207554ab742
nburnet1 <nburnet1@unca.edu>
2023-02-03 10:46:09 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   lectures/lecture07/02-attribute-demo/main.js

--- lectures/lecture07/02-attribute-demo/main.js
+++ lectures/lecture07/02-attribute-demo/main.js
@@ -1,9 +1,9 @@
 /* 
 cat:  https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg
 dog:  https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg
-bird: https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg
-fish: https://thumbs-prod.si-cdn.com/n7Z82GD9Eav_CtpnzizNo66-dKc=/420x240/https://public-media.si-cdn.com/filer/d6/93/d6939718-4e41-44a8-a8f3-d13648d2bcd0/c3npbx.jpg'
-*/
+const birsd 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/1200px-Eopsaltria_australis_-_Mogo_Campground.jpg'
+const fish = 'https://thumbs-prod.si-cdn.com/n7Z82GD9Eav_CtpnzizNo66-dKc=/420x240/https://public-media.si-cdn.com/filer/d6/93/d6939718-4e41-44a8-a8f3-d13648d2bcd0/c3npbx.jpg'
+
 
 
 const showCat = (ev) => {
@@ -14,6 +14,7 @@ const showCat = (ev) => {
 const showDog = (ev) => {
     // your code here...
     console.log('update the image to show a dog!');
+
 };
 
 const showBird = (ev) => {
