188993ea9c042e9ddd941b1c1711e9aac31554f9
Noah Burnette <nburnet1@unca.edu>
2023-02-09 15:11:20 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   homework/hw03/carousel.js
    	modified:   homework/hw03/index.html

--- homework/hw03/carousel.js
+++ homework/hw03/carousel.js
@@ -41,40 +41,78 @@ const data = [
   }
 ];
 
-function template(data, count){
+const carousel = {
+  index: 1,
+  caroLength: data.length,
+  getIndex: function () {
+    return this.index;
+  },
+  setIndex: function (num) {
+    this.index = num;
+  },
+  getCaroLength: function () {
+    return this.caroLength;
+  },
+
+
+  next: function () {
+    const inner = document.querySelector(".carousel-inner");
+    const slide = document.querySelector(".slide");
+    const slideWidth = slide.clientWidth;
+
+    document.getElementById(this.getIndex()).setAttribute("aria-hidden", 'true');
+
+    if (this.getIndex() < this.getCaroLength()) {
+      inner.style.left = `-${slideWidth * (this.getIndex())}px`;
+      this.setIndex(this.getIndex() + 1);
+
+    }
+    else {
+      this.setIndex(1);
+      inner.style.left = `-${slideWidth * (this.getIndex() - 1)}px`;
+    }
+
+    document.getElementById(this.getIndex()).setAttribute("aria-hidden", 'false');
+  },
+
+  previous: function () {
+    const inner = document.querySelector(".carousel-inner");
+    const slide = document.querySelector(".slide");
+    const slideWidth = slide.clientWidth;
+
+    document.getElementById(this.getIndex()).setAttribute("aria-hidden", 'true');
+
+    if (this.getIndex() > 1) {
+      this.setIndex(this.getIndex() - 1);
+      inner.style.left = `-${slideWidth * (this.getIndex() - 1)}px`;
+    }
+    else {
+      this.setIndex(10);
+      inner.style.left = `-${slideWidth * (this.getIndex() - 1)}px`;
+    }
+
+    document.getElementById(this.getIndex()).setAttribute("aria-hidden", 'false');
+  }
+
+};
+
+function template(data, count) {
   return `
-  <section class="slide" index="${count}" role="group" aria-label="Slide ${count + 1} of 10">
+  <section class="slide" id="${count + 1}" role="group" aria-hidden="true" aria-label="Slide ${count + 1} of 10">
   <img src="${data.image_url}" alt="Photo ${count}">
   <p>${data.caption}</p>
   </section>
   `
 }
 
+function loadslides(photoList) {
+  for (let i = 0; i < data.length; i++) {
+    document.querySelector('.carousel-inner').insertAdjacentHTML('beforeend', template(data[i], i));
+  }
 
-
-
-
-
-function next(){
-  const inner = document.querySelector(".carousel-inner");
-  const slide = document.querySelector(".slide");
-  const slideWidth = slide.clientWidth;
-
-  //alert(slide.getAttribute("index"));
-
-  inner.style.left = `-${slideWidth * (slide.getAttribute("index") + 1)}px`;
-
-  slide.style.display = "none";
-  slide.ariaHidden = "true";
-  
-  
-
+  document.querySelector(".slide").setAttribute("aria-hidden", "false");
 }
 
-function previous(){
+loadslides(data);
 
-}
 
-for (let i = 0; i < data.length; i ++){
-  document.querySelector('.carousel-inner').insertAdjacentHTML('beforeend',template(data[i],i));
-}
